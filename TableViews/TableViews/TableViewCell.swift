5d27fc8b9972d6915af93b4fd0066a2307bc42be
ahmad1aa <Ahmed.Atallah.has.s@tuwaiq.edu.sa>
2021-10-31 16:01:23 +0300

    done homework tabels

---
+++ TableViews/TableViews/TableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  TableViewCell.swift
+//  TableViews
+//
+//  Created by Ahmed Alenazi on 25/03/1443 AH.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
