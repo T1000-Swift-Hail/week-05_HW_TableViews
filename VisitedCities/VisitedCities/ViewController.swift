4db6c128e2429c887601d51c3ef3b7ca0b8a6abd
Majed Alshammari <majed.wanis.has.s@tuwaiq.edu.sa>
2021-10-31 15:00:05 +0300

    TableView done

---
+++ VisitedCities/VisitedCities/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  VisitedCities
+//
+//  Created by Majed Alshammari on 25/03/1443 AH.
+//
+
+import UIKit
+
+//class ViewController: UIViewController {
+
+    
+    
+    
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        // Do any additional setup after loading the view.
+//    }
+//
+//
+//}
+
