841fc7acab9b49877fabae0ef0dcb1e8b6c014bd
Njoud Alrshidi <nujud.abdulaziz.has.s@tuwaiq.edu.sam>
2021-10-31 14:56:28 +0300

    Done

---
+++ HW_TableViews/HW_TableViews/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  hw_TableViews
+//
+//  Created by Njoud Alrshidi on 25/03/1443 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
