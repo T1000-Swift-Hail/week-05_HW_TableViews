4fc8ad719e43cbe14281d8fd2f61dfc99241fc25
azoz <aabdoalaziz1@gmail.com>
2021-10-31 16:04:23 +0300

    HW_TableViews is Done

---
+++ HW_TableViews/HW_TableViews/TableViewCellmine.swift
@@ -0,0 +1,23 @@
+//
+//  TableViewCellmine.swift
+//  HW_TableViews
+//
+//  Created by Mac on 31/10/2021.
+//
+
+import UIKit
+
+class TableViewCellmine: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ HW_TableViews/HW_TableViewsTests/HW_TableViewsTests.swift
@@ -0,0 +1,33 @@
+//
+//  HW_TableViewsTests.swift
+//  HW_TableViewsTests
+//
+//  Created by Mac on 31/10/2021.
+//
+
+import XCTest
+@testable import HW_TableViews
+
+class HW_TableViewsTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
