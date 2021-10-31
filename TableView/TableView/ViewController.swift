fc12654bfe79834bb972eca2ec53158d0abb3531
MACBOOK <macbook@Az-MacBook-Pro.local>
2021-10-31 15:32:19 +0300

    Done The HW

---
+++ TableView/TableView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TableView
+//
+//  Created by MACBOOK on 25/03/1443 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
