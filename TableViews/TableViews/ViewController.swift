81b5b33906f17dbf71bb3e5bbd6ec0b2ba4c338d
Hind Aljehani <Hind.Aljehani.has.s@tuwaiq.edu.sa>
2021-10-31 16:37:55 +0300

    Done Hw

---
+++ TableViews/TableViews/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  TableViews
+//
+//  Created by HIND12 on 25/03/1443 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+ override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
