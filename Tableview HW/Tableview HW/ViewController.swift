22c80398e90fe318d0686eadd71117f492c35ba7
AsmaRashed1 <Asma.Rashed.has.s@tuwaiq.edu.sa>
2021-10-31 14:53:04 +0300

    Dooone

---
+++ Tableview HW/Tableview HW/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tableview HW
+//
+//  Created by Asma Rasheed on 25/03/1443 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
