3a8f4ef6c0d7b89c9f9d15962d5fd12f9023c21d
موضي الحربي <modhialhrbi@MacBook-Air-mwdy.local>
2021-10-31 15:06:25 +0300

    DN HW

---
+++ tableviews cities/tableviews cities/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  tableviews cities
+//
+//  Created by موضي الحربي on 25/03/1443 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ tableviews cities/tableviews citiesUITests/tableviews_citiesUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  tableviews_citiesUITestsLaunchTests.swift
+//  tableviews citiesUITests
+//
+//  Created by موضي الحربي on 25/03/1443 AH.
+//
+
+import XCTest
+
+class tableviews_citiesUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
