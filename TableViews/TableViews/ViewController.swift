56e849342c831713391e208f0851ba6b920fe89f
Munira abdullah <muniraabdullah@MacBook-alkhas-b-Munira.local>
2021-10-31 14:52:54 +0300

    Done HW

---
+++ TableViews/TableViews/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TableViews
+//
+//  Created by Munira abdullah on 25/03/1443 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
