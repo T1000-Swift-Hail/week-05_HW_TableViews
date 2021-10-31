11f54dfc4c67eefdef75e1c4b32d758348613d5f
Anas Hamad <anashamad@MacBook-Air-Anas.local>
2021-10-31 14:53:06 +0300

    Cities want to visit dis

---
+++ Cities/Cities/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Cities
+//
+//  Created by Anas Hamad on 25/03/1443 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
