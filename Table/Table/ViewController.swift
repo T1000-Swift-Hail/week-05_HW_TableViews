9f33157eed4c61b871ac6f1ea28bc7878d30f91a
Tlal <talalalglaadi@MacBook-Air-tlal.local>
2021-10-31 16:26:00 +0300

    EX Table Done

---
+++ Table/Table/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  Table
+//
+//  Created by طلال عبيدالله دعيع القلادي on 31/10/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    
+    @IBOutlet weak var tableView: UITableView!
+    let CitiesVisited = ["Abha","Riyadh","Makkah"]
+    let CitiesNext = ["Tabuk","Jeddah","Khobar"]
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Table/Table/ViewTableController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewTableController.swift
+//  Table
+//
+//  Created by طلال عبيدالله دعيع القلادي on 31/10/2021.
+//
+
+import Foundation
+import UIKit
+class ViewTableController: UITableViewController {
+    
+    @IBOutlet weak var CitiesName: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
