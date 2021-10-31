6574a71582f2984b36b331bf5b75a762c1833d89
monaaleid <92249905+monaaleid@users.noreply.github.com>
2021-10-31 15:26:46 +0300

    Done

---
+++ HW_TableViews/HW_TableViews/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW_TableViews
+//
+//  Created by mona aleid on 25/03/1443 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
