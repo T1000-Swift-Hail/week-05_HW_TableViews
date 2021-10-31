0572991ffed51e9acfd407f1fdf49b9edb8bb570
noyer altamimi <noyer.faisal.has.s@tuwaiq.edu.sa>
2021-10-31 15:30:56 +0300

    done

---
+++ Cities/Cities/HomeTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  HomeTableViewCell.swift
+//  Cities
+//
+//  Created by noyer altamimi on 25/03/1443 AH.
+//
+
+import UIKit
+
+class HomeTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Cities/Cities/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Cities
+//
+//  Created by noyer altamimi on 25/03/1443 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+
+    
+    
+    
+    }
+
+
+}
+
+
