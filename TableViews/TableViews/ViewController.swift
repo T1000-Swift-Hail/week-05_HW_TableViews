ba399ef661953e78c9a0ed9710d82535ae95cad0
Mohammed Abdullah <mona@MacBook-Pro-mohammed.local>
2021-10-31 03:33:12 -0700

    Done The Homework -TableViews

---
+++ TableViews/TableViews/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TableViews
+//
+//  Created by Mohammed Abdullah on 25/03/1443 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
